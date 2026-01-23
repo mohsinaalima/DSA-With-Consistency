ef8cc44b44672aa02ca40eeb64ee7e9b8b74d3bd
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-01-23 16:59:12 +0530

    loop q2

--- LOOPS/q2.cpp
+++ LOOPS/q2.cpp
@@ -1,7 +1,16 @@
+// Write a program in C++ to find the sum of the first 10 natural numbers.
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    int sum = 0;
 
+    for(int i = 1; i<=10; i++)
+    {
+        sum = sum + i;
+
+    }
+
+    cout <<  sum << endl ;
 }
