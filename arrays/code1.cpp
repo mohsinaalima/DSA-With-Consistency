3df27166ceba28b8e368c2d6ac21a7c14bf5c55a
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-27 01:25:45 +0530

    code1.cpp

---
+++ arrays/code1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    //declaring 2D array
+   // int arr[10] [15];  
+
+   //initialise
+   int arr[5][2] = {
+   {10,30},
+   {20,60},
+   {22,44},
+   {23,45},
+   {65,77}
+   };
+ 
+   cout << arr[2][1] << endl;
+
+    return 0;
+}
