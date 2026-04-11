d61142511ea2f88c097b9f8c82bc29469d9c65a4
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-04-11 23:43:27 +0530

    feat: find duplicate number using Floyd cycle detection

---
+++ q54.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,3,4,2,2};
+
+    int slow = arr[0], fast = arr[0];
+
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while(slow != fast);
+
+    slow = arr[0];
+
+    while(slow != fast){
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    cout << slow;
+}
