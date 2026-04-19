5f754ececceacff319c47c3299c0a1bb41a6b9aa
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-04-19 22:56:59 +0530

    feat: print vector elements using iterators

---
+++ q62.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40};
+
+    for(vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << *it << " ";
+    }
+}
