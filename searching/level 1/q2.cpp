a113a5a6029bb4d43d2be922107cd354a614b6d7
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-06-03 13:53:53 +0530

    Implemented digit counting using while loop

---
+++ searching/level 1/q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cin >> num;
+    
+    int count = 0;
+
+    while(num > 0)
+{
+    count++;
+    num = num / 10;
+
+}
+cout << count;
+
+return 0;
+}
