1afc52132904b4fddd1155e5ac42d225a089d1ba
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-06-06 20:06:46 +0530

    Find Largest Element in Array

---
+++ searching/q7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+
+    for(int i = 1; i < 5; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest = " << largest;
+
+    return 0;
+}
