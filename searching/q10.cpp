51abf947fe1a0c49d34ce8ce8b087d8de8541b62
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-06-14 23:25:33 +0530

    feat: implement jump game using greedy approach
    - Tracked farthest reachable index
    - Detected unreachable positions
    - Achieved O(n) complexity

---
+++ searching/q10.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool wordBreak(string s, vector<string>& wordDict) {
+
+    unordered_set<string> dict(
+        wordDict.begin(),
+        wordDict.end()
+    );
+
+    vector<bool> dp(
+        s.size() + 1,
+        false
+    );
+
+    dp[0] = true;
+
+    for(int i = 1; i <= s.size(); i++) {
+
+        for(int j = 0; j < i; j++) {
+
+            if(dp[j] &&
+               dict.count(
+                   s.substr(j, i-j)
+               )) {
+
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[s.size()];
+}
+
+int main() {
+
+    string s = "leetcode";
+
+    vector<string> dict = {
+        "leet",
+        "code"
+    };
+
+    cout << wordBreak(s, dict);
+}
