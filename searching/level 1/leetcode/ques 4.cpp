54ebc267b1222d54500036f6c7936bfe1208e92c
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-07-06 21:12:08 +0530

    feat: remove duplicates from sorted array

---
+++ searching/level 1/leetcode/ques 4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+
+    if (nums.empty())
+        return 0;
+
+    int index = 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+
+        if (nums[i] != nums[i - 1]) {
+            nums[index++] = nums[i];
+        }
+    }
+
+    return index;
+}
