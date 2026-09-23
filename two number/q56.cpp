4586077b331000d0e16935e3692f46abed8c6efd
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-09-23 22:29:47 +0530

    feat: add spiral matrix solution

---
+++ two number/q56.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> ans;
+
+    int top = 0;
+    int bottom = matrix.size() - 1;
+    int left = 0;
+    int right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+
+        // Left -> Right
+        for (int j = left; j <= right; j++) {
+            ans.push_back(matrix[top][j]);
+        }
+        top++;
+
+        // Top -> Bottom
+        for (int i = top; i <= bottom; i++) {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        // Right -> Left
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                ans.push_back(matrix[bottom][j]);
+            }
+            bottom--;
+        }
+
+        // Bottom -> Top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    vector<int> ans = spiralOrder(matrix);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
